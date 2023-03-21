13320c6ea5e35acd9bf1e0a473751c59cd25d29d
antonio <ammj24@hotmail.com>
2023-03-21 22:01:38 +0000

    conexion BBDD

--- src/main/java/modelo/ConexionBBDD.java
+++ src/main/java/modelo/ConexionBBDD.java
@@ -7,18 +7,18 @@ import java.util.Properties;
 
 public class ConexionBBDD {
 	
-	public static final String JDBC_URL = "jdbc:mysql://localhost:3306/consultaonline";
-//	public static final String JDBC_URL = "jdbc:mysql://eu-cdbr-west-03.cleardb.net/heroku_40029d4180b186d?autoReconnect=true";
+//	public static final String JDBC_URL = "jdbc:mysql://localhost:3306/consultaonline";
+	public static final String JDBC_URL = "jdbc:mysql://eu-cdbr-west-03.cleardb.net/heroku_40029d4180b186d?autoReconnect=true";
 	public static Connection instance = null;
 	
 	public static Connection getConnection() throws SQLException {
 		
 		if(instance == null) {
 			Properties props = new Properties();
-//			props.put("user", "ba840f90deec6a");
-//			props.put("password", "bfefe5fd");
-			props.put("user", "root");
-			props.put("password", "root");
+			props.put("user", "ba840f90deec6a");
+			props.put("password", "bfefe5fd");
+//			props.put("user", "root");
+//			props.put("password", "root");
 			
 			instance = DriverManager.getConnection(JDBC_URL, props);
 		}
