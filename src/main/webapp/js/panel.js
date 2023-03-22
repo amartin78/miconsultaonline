4d5d16b9cc4647c287a4636dab2626cc8d3c5e20
antonio <ammj24@hotmail.com>
2023-03-22 10:30:59 +0000

    contenido prueba js

--- src/main/webapp/js/panel.js
+++ src/main/webapp/js/panel.js
@@ -1,25 +1,9 @@
 window.onload = function () {
-	
     const e = document.createElement("h1");
     document.getElementById("contenido-principal").appendChild(e).innerText = "Perfil";
-    
-    fetch('ObtenerPaciente').
-	then(response => response.json()).
-	then(data => {
-		paciente = data;
-		nombreCompleto = paciente["nombre"] + " " + paciente["apellidos"];
-	    document.getElementById("paciente").getElementsByTagName("button")[0].innerText = nombreCompleto;
-	});
-	
-	document.getElementById("paciente").getElementsByTagName("button")[0].addEventListener('click', function() {
-		// alert("cierre de sesion");
-		
-	});
-	
 };
 
 function opcion(o) {
-	
     let nombreOpcion = o.innerText;
     document.getElementById("contenido-principal").
              getElementsByTagName("h1")[0].innerText = nombreOpcion;
@@ -56,8 +40,37 @@ function diagnostico() {
 
     limpiarContenidoPrincipal();
     let contenidoPrincipal = document.getElementById("contenido-principal").innerHTML;
-    let datosDiagnostico = [["Dolor muscular", "04/09/2021"], 
-                            ["Grano en la cara", "24/11/2021"]];
+    let datosDiagnostico = [["Dolor muscular 1", "04/01/2021"], 
+                            ["Dolor muscular 2", "04/02/2021"],
+                            ["Dolor muscular 3", "04/03/2021"], 
+                            ["Dolor muscular 4", "04/04/2021"], 
+                            ["Dolor muscular 5", "04/05/2021"], 
+                            ["Dolor muscular 6", "04/06/2021"], 
+                            ["Dolor muscular 7", "04/07/2021"], 
+                            ["Dolor muscular 8", "04/08/2021"], 
+                            ["Dolor muscular 9", "04/09/2021"], 
+                            ["Dolor muscular 10", "04/10/2021"], 
+                            ["Dolor muscular 11", "04/11/2021"], 
+                            ["Dolor muscular 12", "04/12/2021"], 
+                            ["Dolor muscular 13", "05/01/2021"], 
+                            ["Dolor muscular 14", "05/02/2021"], 
+                            ["Dolor muscular 15", "05/03/2021"],
+                            ["Dolor muscular 1", "04/01/2021"], 
+                            ["Dolor muscular 2", "04/02/2021"],
+                            ["Dolor muscular 3", "04/03/2021"], 
+                            ["Dolor muscular 4", "04/04/2021"], 
+                            ["Dolor muscular 5", "04/05/2021"], 
+                            ["Dolor muscular 6", "04/06/2021"], 
+                            ["Dolor muscular 7", "04/07/2021"], 
+                            ["Dolor muscular 8", "04/08/2021"], 
+                            ["Dolor muscular 9", "04/09/2021"], 
+                            ["Dolor muscular 10", "04/10/2021"], 
+                            ["Dolor muscular 11", "04/11/2021"], 
+                            ["Dolor muscular 12", "04/12/2021"], 
+                            ["Dolor muscular 13", "05/01/2021"], 
+                            ["Dolor muscular 14", "05/02/2021"], 
+                            ["Dolor muscular 15", "05/03/2021"] 
+                        ];
     let tablaDiagnostico = "<table border='1'>";
     tablaDiagnostico += "<tr><th>Anomalía</th><th>Fecha consulta</th></tr>";
     for(let i = 0; i < datosDiagnostico.length; i++) {
