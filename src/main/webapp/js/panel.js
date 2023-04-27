b7c53c7dc8a0b7acd9cbd7a022bb50eb2df43ed5
antonio <ammj24@hotmail.com>
2023-04-27 21:48:46 +0200

    test conexion 22

--- src/main/webapp/js/panel.js
+++ src/main/webapp/js/panel.js
@@ -58,7 +58,7 @@ window.onload = function () {
 	
 	// Se comprueba cada medio minuto que la cookie este activa, en caso contrario se redirecciona al  
 	// cliente a la página de loguin para autenticarse de nuevo e iniciar una nueva sesión.
-	setInterval(checkCookie, 30000);
+	setInterval(checkCookie, 2000);
 };
 
 // Redirecciona al cliente a la página de loguin solo en el caso 
