6d6bc33c375afc839918488335f81e6200ecaa86
antonio <ammj24@hotmail.com>
2023-04-30 13:57:01 +0200

    Se modifica layout tablas analíticas

--- src/main/webapp/js/includes/analisis.js
+++ src/main/webapp/js/includes/analisis.js
@@ -15,46 +15,51 @@ function analisis() {
 		if(analisis) {
 			for(let a in analisis) {
 				if(analisis.hasOwnProperty(a) && analisis[a]["marcadores"].length > 0) {
-					// console.log(analisis[a])				
 					
+					tablaAnalisis += "<div class='contenedor-tabla-analisis'>";
 		    		tablaAnalisis += "<table class='listado-analisis'>";
 		    		
 					let nombre = analisis[a]["nombre"] == undefined ? "" : analisis[a]["nombre"];
-			        let estado = analisis[a]["estado"] == undefined ? "" : analisis[a]["estado"];
 			        let fecha = analisis[a]["fecha"] == undefined ? "" : analisis[a]["fecha"];
 			        if(fecha !== undefined) {
 						fecha = new Date(fecha);
-				        let anio = fecha.getFullYear();
-				        let mes = fecha.getMonth() + 1;
-				        mes = String(mes).length == 2 ? mes : `0${mes}`;
-				        let dia = fecha.getDate();
-				        dia = String(dia).length == 2 ? dia : `0${dia}`;
-				        fecha = anio + "-" + mes + "-" + dia;
+						const options = {day: "numeric", month: "long", year: "numeric"};
+						fecha = new Intl.DateTimeFormat("es-ES", options).format(fecha);
 					}
-
+					
+					// Encabezado tabla con el tipo de analítica y la fecha
+					tablaAnalisis += "<tr><td>" + nombre + "</td>" +
+									 "<td></td>" +
+									 "<td></td>" +
+									 "<td></td>" +  
+							         "<td>" + fecha + "</td></tr>";
+					
+					// Nombre columnas							                    
+					tablaAnalisis += "<tr><th>Nombre</th>" + 
+					                     "<th>Valor</th>" +
+					                     "<th>Valor mínimo</th>" +
+					                     "<th>Valor máximo</th>" +
+					                     "<th>Resultado</th></tr>";
+							                     
 					let marcadores = analisis[a]["marcadores"]; 
 					for(let m in marcadores) {
 						if(marcadores.hasOwnProperty(m) && marcadores.length > 0) {
 							nombre = marcadores[m]["nombre"] == undefined ? "" : marcadores[m]["nombre"];
-							let categoria = marcadores[m]["categoria"] == undefined ? "" : marcadores[m]["categoria"];
 							let valor = marcadores[m]["valor"] == undefined ? "" : marcadores[m]["valor"];
 					        let valorMinimo = marcadores[m]["valorMinimo"] == undefined ? "" : marcadores[m]["valorMinimo"];
 					        let valorMaximo = marcadores[m]["valorMaximo"] == undefined ? "" : marcadores[m]["valorMaximo"];
 					        let resultado = marcadores[m]["resultado"] == undefined ? "" : marcadores[m]["resultado"];
 							tablaAnalisis += "<tr><td>" + nombre + "</td>" + 
-							                     "<td>" + fecha + "</td>" +
-							                     "<td>" + categoria + "</td>" +
 							                     "<td>" + valor + "</td>" +
 							                     "<td>" + valorMinimo + "</td>" +
 							                     "<td>" + valorMaximo + "</td>" +
 							                     "<td><span id='resultado'>" + resultado + "</span></td></tr>";
 						}
 					}
-					
 					tablaAnalisis += "</table>";
+					tablaAnalisis += "</div>";
 				}
 			}
-			
 			tablaAnalisis += "</div>";
 			
 	    	document.getElementById("contenido-principal").innerHTML += tablaAnalisis;
